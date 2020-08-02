619bb7611206528a90f1e8f0b858796851c6fce3
miguemesen <miguemesen12@gmail.com>
2020-08-01 19:02:11 -0600

    Interface edit

--- out/artifacts/Proyecto3/Compra.jsp
+++ out/artifacts/Proyecto3/Compra.jsp
@@ -6,6 +6,8 @@
 
 <h2> Pagina de compra</h2>
 
+
+Ponemos unos input donde metan el correo y la informacion para que hagan el pago
 <form >
     <input type="submit" value="BuyTicket">
 </form>
---
+++ src/cr/ac/tec/Classes/EstacionesRutas.java
@@ -0,0 +1,8 @@
+package cr.ac.tec.Classes;
+
+import java.util.ArrayList;
+
+public class EstacionesRutas {
+    ArrayList<Nodes> nodes = new ArrayList<Nodes>();//Estas son las estacioness
+    ArrayList<Edges> edges = new ArrayList<Edges>();//Estas son las rutas
+}
