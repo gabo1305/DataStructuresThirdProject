811ad51dc90b00fa5adb88e6b52f6da8b72c1a9a
Gabriel Solano Coronado <gabrielsolcor@estudiantec.cr>
2020-08-06 11:04:09 -0600

    [RAIL-14] Finish login to users

--- src/cr/ac/tec/Servlets/Login.java
+++ src/cr/ac/tec/Servlets/Login.java
@@ -2,6 +2,7 @@ package cr.ac.tec.Servlets;
 
 import cr.ac.tec.Rail.Accounts.User;
 import cr.ac.tec.SavedInfo.UsersTree;
+import cr.ac.tec.Verification.UserVerification;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,28 +11,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.Console;
 import java.io.IOException;
-//@WebServlet(name = "LoginServlet",value = "/log")
-@WebServlet("/log")
+@WebServlet(name = "LoginServlet",value = "/log")
 public class Login extends HttpServlet {
     private final String name="UserName";
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String userID=req.getParameter(name);
+        if(!UserVerification.ced(userID)){
+            resp.sendRedirect("login.jsp");
+            return;
+        }
+
         User user=new User(userID);
         UsersTree tree=UsersTree.getInstance();
         tree.attach(user);
         user=tree.getMember(user);
         resp.getWriter().print(user.getID());
-        System.out.println();
-        if(userID.equals("123")){
-
-            resp.sendRedirect("prueba.html");
-        }
-        else{
-            resp.sendRedirect("login.jsp");
-        }
+        resp.sendRedirect("prueba.html");
     }
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req,resp);
