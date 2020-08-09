2019b9df805dffa5eed165521e1f6a284b24a2fb
lazhero <54720338+lazhero@users.noreply.github.com>
2020-08-09 09:37:01 -0600

    [RAIL-7] Adding the get Price

---
+++ src/cr/ac/tec/Servlets/GetPrice.java
@@ -0,0 +1,27 @@
+package cr.ac.tec.Servlets;
+
+import cr.ac.tec.Rail.RequestManager.RequestManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/getPrice")
+public class GetPrice extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String Start=req.getParameter("Start");
+        String End=req.getParameter("End");
+        RequestManager requestManager=RequestManager.getInstance();
+        double price=requestManager.getPrice(Start,End);
+        resp.getWriter().print(price);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+}
---
+++ src/cr/ac/tec/Servlets/TicketsFromNode.java
@@ -0,0 +1,31 @@
+package cr.ac.tec.Servlets;
+
+import cr.ac.tec.FileProccessing.JsonExchange;
+import cr.ac.tec.Rail.Purchase.Ticket;
+import cr.ac.tec.Rail.RequestManager.RequestManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet(value = "/ConsultNode")
+public class TicketsFromNode extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer id=Integer.parseInt(req.getParameter("Node"));
+        System.out.println(id);
+        RequestManager requestManager=RequestManager.getInstance();
+        ArrayList<Ticket> List=requestManager.getTicketByNode(id);
+        String re=JsonExchange.getStringFromObject(List);
+        resp.getWriter().print(re);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+}
