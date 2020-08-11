4e0972136662104dc0a1b0fc176de425685c8d15
lazhero <54720338+lazhero@users.noreply.github.com>
2020-08-10 21:11:49 -0600

    [RAIL-7] ID

---
+++ src/cr/ac/tec/Servlets/DeleteStopping.java
@@ -0,0 +1,26 @@
+package cr.ac.tec.Servlets;
+
+import cr.ac.tec.Rail.RequestManager.RequestManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/deleteNode")
+public class DeleteStopping extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String Node=req.getParameter("NodeID");
+        RequestManager requestManager=RequestManager.getInstance();
+        if(!requestManager.deleteNode(Node))resp.getWriter().print("Habia tickets en el nodo");
+        else resp.getWriter().print("Lo logre");
+    }
+}
